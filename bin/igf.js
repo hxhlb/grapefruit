187c7998b323bb9633e878fd9fdeea77fb8867de
cc <codecolorist@gmail.com>
2025-06-18 23:38:17 +0200

    dynamically import next config

---
+++ bin/igf.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node --env-file-if-exists=.env
+
+import "../server/web.ts";
--- server/web.ts
+++ server/web.ts
@@ -3,33 +3,43 @@ import path from "node:path";
 
 import next from "next";
 
-import conf from "../gui/next.config.ts";
 import app from "./app.ts";
 import factory from "./io.ts";
 import env from "./lib/env.ts";
 
-// todo: wait for next.js issue to be resolved
-// https://github.com/vercel/next.js/issues/46078
-// @ts-ignore
-const nextApp = next({
+const dir = path.join(import.meta.dirname, "..", "gui");
+const distDir = path.join(dir, ".next");
+
+const opt = {
+  turbo: true,
   dev: env.dev,
-  dir: path.join(import.meta.dirname, "..", "gui"),
-  distDir: path.join(import.meta.dirname, "..", "gui", ".next"),
-  conf,
-});
+  dir,
+  distDir,
+};
 
 const koaHandler = app.callback();
-const handler = nextApp.getRequestHandler();
-
-function handleRequest(req: http.IncomingMessage, res: http.ServerResponse) {
-  if (req.url?.startsWith("/api")) {
-    koaHandler(req, res);
-  } else {
-    handler(req, res);
-  }
-}
 
 export default async function serve() {
+  // use ts-ignore for a workaround for this issue:
+  // https://github.com/vercel/next.js/issues/46078
+
+  if (env.dev) {
+    // @ts-ignore
+    opt.conf = await import("../gui/next.config.ts");
+  }
+
+  // @ts-ignore
+  const nextApp = next(opt);
+  const handler = nextApp.getRequestHandler();
+
+  function handleRequest(req: http.IncomingMessage, res: http.ServerResponse) {
+    if (req.url?.startsWith("/api")) {
+      koaHandler(req, res);
+    } else {
+      handler(req, res);
+    }
+  }
+
   await nextApp.prepare();
   const server = http.createServer(handleRequest);
   factory(server);
