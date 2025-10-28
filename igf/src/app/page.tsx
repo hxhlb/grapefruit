54e4ab8e3a48588e082f9117f3d681b7ac511503
cc <codecolorist@gmail.com>
2025-10-28 18:43:39 +0100

    next.js 16

--- igf/src/app/page.tsx
+++ igf/src/app/page.tsx
@@ -5,7 +5,7 @@ import { ThemeToggle } from "@/components/theme-toggle";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] bg-background min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] bg-background min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
         <h1>
           <Image
