5b5c752e5997248faed0d62f2b68ddf0988ee515
cc <codecolorist@gmail.com>
2025-11-03 19:48:29 +0100

    add frontend boilerplate

---
+++ gui/src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="flex flex-wrap items-center gap-2 md:flex-row"></div>
+    </div>
+  );
+}
+
+export default App;
